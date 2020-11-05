d9d578eccbd942627dc777669e0b7ee9bb0a12b1
Sokolover <sa.sokoloff@mail.ru>
2020-11-05 18:10:27 +0300

    updated sql script and png of model

--- appendix/create_database_schema_module3.sql
+++ appendix/create_database_schema_module3.sql
@@ -30,6 +30,50 @@ CREATE TABLE IF NOT EXISTS `module3`.`tag`
 
 
 -- -----------------------------------------------------
+-- Table `module3`.`gift_certificate`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `module3`.`gift_certificate`
+(
+    `id`                         BIGINT(100)   NOT NULL AUTO_INCREMENT,
+    `name`                       VARCHAR(200)  NOT NULL,
+    `description`                VARCHAR(1000) NOT NULL,
+    `price`                      DECIMAL(7, 2) NOT NULL,
+    `create_date`                DATETIME      NOT NULL,
+    `create_date_time_zone`      VARCHAR(50)   NOT NULL,
+    `last_update_date`           DATETIME      NOT NULL,
+    `last_update_date_time_zone` VARCHAR(50)   NOT NULL,
+    `duration`                   INT           NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `module3`.`tag_has_gift_certificate`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `module3`.`tag_has_gift_certificate`
+(
+    `tag_id`              BIGINT(100) NOT NULL,
+    `gift_certificate_id` BIGINT(100) NOT NULL,
+    PRIMARY KEY (`tag_id`, `gift_certificate_id`),
+    INDEX `fk_tag_has_gift_certificate_gift_certificate1_idx` (`gift_certificate_id` ASC) VISIBLE,
+    INDEX `fk_tag_has_gift_certificate_tag_idx` (`tag_id` ASC) VISIBLE,
+    CONSTRAINT `fk_tag_has_gift_certificate_tag`
+        FOREIGN KEY (`tag_id`)
+            REFERENCES `module3`.`tag` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    CONSTRAINT `fk_tag_has_gift_certificate_gift_certificate1`
+        FOREIGN KEY (`gift_certificate_id`)
+            REFERENCES `module3`.`gift_certificate` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `module3`.`user_account`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `module3`.`user_account`
@@ -70,48 +114,21 @@ CREATE TABLE IF NOT EXISTS `module3`.`user_order`
 
 
 -- -----------------------------------------------------
--- Table `module3`.`gift_certificate`
+-- Table `module3`.`user_order_has_gift_certificate`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `module3`.`gift_certificate`
+CREATE TABLE IF NOT EXISTS `module3`.`user_order_has_gift_certificate`
 (
-    `id`                         BIGINT(100)   NOT NULL AUTO_INCREMENT,
-    `name`                       VARCHAR(200)  NOT NULL,
-    `description`                VARCHAR(1000) NOT NULL,
-    `price`                      DECIMAL(7, 2) NOT NULL,
-    `creat_date`                 DATETIME      NOT NULL,
-    `create_date_time_zone`      VARCHAR(50)   NOT NULL,
-    `last_update_date`           DATETIME      NOT NULL,
-    `last_update_date_time_zone` VARCHAR(50)   NOT NULL,
-    `duration`                   INT           NOT NULL,
-    `user_order_id`              BIGINT(100)   NOT NULL,
-    PRIMARY KEY (`id`, `user_order_id`),
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-    INDEX `fk_gift_certificate_user_order1_idx` (`user_order_id` ASC) VISIBLE,
-    CONSTRAINT `fk_gift_certificate_user_order1`
+    `user_order_id`       BIGINT(100) NOT NULL,
+    `gift_certificate_id` BIGINT(100) NOT NULL,
+    PRIMARY KEY (`user_order_id`, `gift_certificate_id`),
+    INDEX `fk_user_order_has_gift_certificate_gift_certificate1_idx` (`gift_certificate_id` ASC) VISIBLE,
+    INDEX `fk_user_order_has_gift_certificate_user_order1_idx` (`user_order_id` ASC) VISIBLE,
+    CONSTRAINT `fk_user_order_has_gift_certificate_user_order1`
         FOREIGN KEY (`user_order_id`)
             REFERENCES `module3`.`user_order` (`id`)
             ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `module3`.`tag_has_gift_certificate`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `module3`.`tag_has_gift_certificate`
-(
-    `tag_id`              BIGINT(100) NOT NULL,
-    `gift_certificate_id` BIGINT(100) NOT NULL,
-    PRIMARY KEY (`tag_id`, `gift_certificate_id`),
-    INDEX `fk_tag_has_gift_certificate_gift_certificate1_idx` (`gift_certificate_id` ASC) VISIBLE,
-    INDEX `fk_tag_has_gift_certificate_tag_idx` (`tag_id` ASC) VISIBLE,
-    CONSTRAINT `fk_tag_has_gift_certificate_tag`
-        FOREIGN KEY (`tag_id`)
-            REFERENCES `module3`.`tag` (`id`)
-            ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    CONSTRAINT `fk_tag_has_gift_certificate_gift_certificate1`
+    CONSTRAINT `fk_user_order_has_gift_certificate_gift_certificate1`
         FOREIGN KEY (`gift_certificate_id`)
             REFERENCES `module3`.`gift_certificate` (`id`)
             ON DELETE NO ACTION
