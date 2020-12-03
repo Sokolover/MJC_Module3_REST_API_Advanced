c16dd829736a4fa519cca2221c9cb0b17edcc2ab
Sokolover <sa.sokoloff@mail.ru>
2020-12-03 17:07:50 +0300

    made separate endpoint for finding most popular tag

--- api/src/main/java/com/epam/esm/sokolov/controller/TagController.java
+++ api/src/main/java/com/epam/esm/sokolov/controller/TagController.java
@@ -1,7 +1,6 @@
 package com.epam.esm.sokolov.controller;
 
 import com.epam.esm.sokolov.dto.TagDTO;
-import com.epam.esm.sokolov.exception.CertificateAppException;
 import com.epam.esm.sokolov.service.tag.TagService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.epam.esm.sokolov.constants.CommonAppConstants.MOST_WIDELY_USED_SEARCH_PARAM;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -25,16 +23,12 @@ public class TagController {
 
     private final TagService tagService;
 
-    @GetMapping
+    @GetMapping("/most-widely-used-tag")
     @ResponseStatus(HttpStatus.OK)
-    public TagDTO findTheMostWidelyUsedTag(String searchFor) {
-        if (MOST_WIDELY_USED_SEARCH_PARAM.equals(searchFor)) {//todo choose endpoint or request param
-            TagDTO tagDTO = tagService.findTheMostWidelyUsedTag();
-            tagDTO.add(linkTo(methodOn(TagController.class)
-                    .findTheMostWidelyUsedTag(searchFor)).withSelfRel());
-            return tagDTO;
-        } else {
-            throw new CertificateAppException("Resource not found", HttpStatus.BAD_REQUEST, this.getClass());
-        }
+    public TagDTO findTheMostWidelyUsedTag() {
+        TagDTO tagDTO = tagService.findTheMostWidelyUsedTag();
+        tagDTO.add(linkTo(methodOn(TagController.class)
+                .findTheMostWidelyUsedTag()).withSelfRel());
+        return tagDTO;
     }
 }
