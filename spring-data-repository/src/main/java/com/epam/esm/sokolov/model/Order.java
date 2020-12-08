ddc1206321c5b53ef5cbb5e33db51fbcf0277f39
Sokolover <sa.sokoloff@mail.ru>
2020-12-08 14:55:59 +0300

    optimized EqualsAndHashCode and ToString with lombok

--- spring-data-repository/src/main/java/com/epam/esm/sokolov/model/Order.java
+++ spring-data-repository/src/main/java/com/epam/esm/sokolov/model/Order.java
@@ -1,16 +1,12 @@
 package com.epam.esm.sokolov.model;
 
 import com.epam.esm.sokolov.model.user.User;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
-import java.util.Objects;
 import java.util.Set;
 
 @Getter
@@ -25,56 +21,40 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private BigDecimal cost;
+    @EqualsAndHashCode.Exclude
     @Column(name = "create_date")
     private LocalDateTime createDate;
     @Column(name = "create_date_time_zone")
     private String createDateTimeZone;
+    @EqualsAndHashCode.Exclude
     @Column(name = "last_update_date")
     private LocalDateTime lastUpdateDate;
     @Column(name = "last_update_date_time_zone")
     private String lastUpdateDateTimeZone;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @ManyToOne
     @JoinColumn(name = "user_account_id")
     private User user;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @ManyToMany
     @JoinTable(
             name = "user_order_has_gift_certificate",
             joinColumns = @JoinColumn(name = "user_order_id"),
             inverseJoinColumns = @JoinColumn(name = "gift_certificate_id"))
     private Set<GiftCertificate> giftCertificates;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private String operation;
     @Column(name = "created_at_time")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private LocalDateTime createdAtTime;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @Column(name = "created_at_time_zone")
     private String createdAtTimeZone;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Order order = (Order) o;
-        return Objects.equals(id, order.id) &&
-                Objects.equals(cost, order.cost) &&
-                Objects.equals(createDateTimeZone, order.createDateTimeZone) &&
-                Objects.equals(lastUpdateDateTimeZone, order.lastUpdateDateTimeZone);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, cost, createDateTimeZone, lastUpdateDateTimeZone);
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", cost=" + cost +
-                ", createDate=" + createDate +
-                ", createDateTimeZone='" + createDateTimeZone + '\'' +
-                ", lastUpdateDate=" + lastUpdateDate +
-                ", lastUpdateDateTimeZone='" + lastUpdateDateTimeZone + '\'' +
-                '}';
-    }
 
     @PrePersist
     public void onPrePersist() {
--- spring-data-repository/src/main/java/com/epam/esm/sokolov/model/Tag.java
+++ spring-data-repository/src/main/java/com/epam/esm/sokolov/model/Tag.java
@@ -1,14 +1,12 @@
 package com.epam.esm.sokolov.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
-import java.util.Objects;
 import java.util.Set;
 
+@EqualsAndHashCode
+@ToString
 @Getter
 @Setter
 @AllArgsConstructor
@@ -21,33 +19,13 @@ public class Tag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @ManyToMany(mappedBy = "tags", cascade = {CascadeType.MERGE})
     private Set<GiftCertificate> giftCertificates;
 
     public Tag(Long id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tag tag = (Tag) o;
-        return Objects.equals(id, tag.id) &&
-                Objects.equals(name, tag.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return "Tag{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
     }
 }
